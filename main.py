e1cdb648432d34c8deccba4a568cfecf38ab362f
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-04 16:34:23 +0200

    Add quiz functionality with question model and brain logic, and include question data and images

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# imports
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-import requests
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-# constants, vars
+quiz = QuizBrain(question_bank)
 
+while quiz.still_has_questions():
+    quiz.next_question()
 
-# functions
-
-
-# main code
-
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
