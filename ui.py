08cf4e58a5207ce4c6d5ba1f17af46dccf7aa3e1
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-04 17:46:25 +0200

    Add tasks.json for running main.py and implement QuizInterface in ui.py

--- ui.py
+++ ui.py
@@ -1,2 +1,24 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20)
+
+        # UI
+
+        score = Label(text="Score: 0")
+        score.grid(column=1, row=0, columnspan=2)
+
+        canvas = Canvas(width=300, height=250, bg="white")
+        canvas.grid(column=1, row=1)
+
+        question_text = canvas.create_text(150, 125, width=100, text="This is a Question", font=("Arial", 20, "italic"))
+
+
+        self.window.mainloop()
